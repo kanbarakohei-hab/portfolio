6ee1d9b6f2f9f0a17b57c3a8453d7c4d6e41d6bf
神原洸平 <62216077+kanbarakohei-hab@users.noreply.github.com>
2020-08-02 05:16:42 +0900

    ポートフォリオ_将棋プログラミング
    将棋プログラミング

---
+++ MotigomaM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KanbaraProject
+{
+    /*駒情報リスト*/
+    class MotigomaM
+    {
+        public static List<MotigomaM_info> 味歩 = new List<MotigomaM_info>();
+        public static List<MotigomaM_info> 味香 = new List<MotigomaM_info>();
+        public static List<MotigomaM_info> 味桂 = new List<MotigomaM_info>();
+        public static List<MotigomaM_info> 味銀 = new List<MotigomaM_info>();
+        public static List<MotigomaM_info> 味金 = new List<MotigomaM_info>();
+        public static List<MotigomaM_info> 味角 = new List<MotigomaM_info>();
+        public static List<MotigomaM_info> 味飛 = new List<MotigomaM_info>();
+
+        public static List<MotigomaM_info> 敵歩 = new List<MotigomaM_info>();
+        public static List<MotigomaM_info> 敵香 = new List<MotigomaM_info>();
+        public static List<MotigomaM_info> 敵桂 = new List<MotigomaM_info>();
+        public static List<MotigomaM_info> 敵銀 = new List<MotigomaM_info>();
+        public static List<MotigomaM_info> 敵金 = new List<MotigomaM_info>();
+        public static List<MotigomaM_info> 敵角 = new List<MotigomaM_info>();
+        public static List<MotigomaM_info> 敵飛 = new List<MotigomaM_info>();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KanbaraProject
+{
+    static class Program
+    {
+        //スタート位置(ここから始まる)
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //アプリケーションで visual スタイルを有効にする(自動生成)
+            Application.EnableVisualStyles();
+
+            //特定のコントロールで定義された UseCompatibleTextRendering プロパティにアプリケーション全体で有効な既定値を設定します。
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            //現在のスレッドで標準のアプリケーション メッセージ ループの実行を開始し、指定したフォームを表示します。
+            //Form1が始まる
+            Application.Run(new Form1());
+        }
+    }
+}
